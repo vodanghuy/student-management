7f19946e6453c6dbd391c83d8bd013f517e4a124
Vo Dang Huy <iamdaghuy@gmail.com>
2025-04-21 03:15:08 +0700

    add subject to student, remove subject from student, get subjects by student

--- src/main/java/com/example/studentmanagement/StudentResource.java
+++ src/main/java/com/example/studentmanagement/StudentResource.java
@@ -1,14 +1,17 @@
 package com.example.studentmanagement;
 
 import com.example.studentmanagement.models.Student;
+import com.example.studentmanagement.models.Subject;
 import com.example.studentmanagement.repositories.StudentRepository;
 import com.example.studentmanagement.services.StudentService;
+import org.springframework.http.HttpCookie;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/students")
@@ -42,5 +45,26 @@ public class StudentResource {
     public ResponseEntity<?> deleteStudent(@PathVariable("id") Long id){
         studentService.deleteStudent(id);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+    // Add subject to student
+    @PostMapping("/{studentId}/subjects/{subjectId}")
+    public ResponseEntity<Student> addSubjectToStudent(@PathVariable("studentId") Long studentId,
+                                                       @PathVariable("subjectId") String subjectId){
+        Student student = studentService.addSubjectToStudent(studentId,subjectId);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+    // Remove subject from student
+    @DeleteMapping("/{studentId}/subjects/{subjectId}")
+    public ResponseEntity<Student> removeSubjectFromStudent(@PathVariable("studentId") Long studentId,
+                                                       @PathVariable("subjectId") String subjectId){
+        Student student = studentService.removeSubjectFromStudent(studentId,subjectId);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    // Get subjects
+    @GetMapping("/{studentId}/subjects")
+    public ResponseEntity<Set<Subject>> getSubjects(@PathVariable("studentId") Long studentId){
+        Set<Subject> subjects = studentService.getSubjects(studentId);
+        return new ResponseEntity<>(subjects, HttpStatus.OK);
     }
 }
--- src/main/java/com/example/studentmanagement/services/StudentService.java
+++ src/main/java/com/example/studentmanagement/services/StudentService.java
@@ -1,21 +1,28 @@
 package com.example.studentmanagement.services;
 
 import com.example.studentmanagement.models.Student;
+import com.example.studentmanagement.models.Subject;
 import com.example.studentmanagement.repositories.StudentRepository;
+import com.example.studentmanagement.repositories.SubjectRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class StudentService {
-    private StudentRepository studentRepository;
+    private final StudentRepository studentRepository;
+    private final SubjectRepository subjectRepository;
 
     @Autowired
-    public StudentService(StudentRepository studentRepository) {
+    public StudentService(StudentRepository studentRepository, SubjectRepository subjectRepository) {
         this.studentRepository = studentRepository;
+        this.subjectRepository = subjectRepository;
     }
+
 
     public List<Student> getAllStudents(){
         return studentRepository.findAll();
@@ -34,5 +41,35 @@ public class StudentService {
     }
     public void deleteStudent(Long id){
         studentRepository.deleteById(id);
+    }
+
+    // Get subjects
+    public Set<Subject> getSubjects(Long studentId){
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new RuntimeException(("Student not found with id: "+ studentId)));
+        return student.getSubjects();
+    }
+    // Add subject to student
+    @Transactional
+    public Student addSubjectToStudent(Long studentId, String subjectId){
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new RuntimeException(("Student not found with id: "+ studentId)));
+        Subject subject = subjectRepository.findById(subjectId)
+                .orElseThrow(() -> new RuntimeException(("Subject not found with id: "+ subjectId)));
+        student.getSubjects().add(subject);
+        subject.setAvailableSlot(subject.getAvailableSlot()-1);
+        subjectRepository.save(subject);
+        return studentRepository.save(student);
+    }
+
+    //Remove subject from student
+    @Transactional
+    public Student removeSubjectFromStudent(Long studentId, String subjectId){
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new RuntimeException(("Student not found with id: "+ studentId)));
+        Subject subject = subjectRepository.findById(subjectId)
+                .orElseThrow(() -> new RuntimeException(("Subject not found with id: "+ subjectId)));
+        student.getSubjects().remove(subject);
+        return studentRepository.save(student);
     }
 }
